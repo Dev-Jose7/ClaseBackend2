b7ea49e8add92067c70faca11ec1306a3c22b8f7
Jose Navarro <jfnr398@gmail.com>
2025-03-18 15:08:13 -0500

    feat Se crea practica 2 password generator y encrypt text

--- Code/practica2passwordgenerator/src/Main.java
+++ Code/practica2passwordgenerator/src/Main.java
@@ -1,15 +1,6 @@
 
 import javax.swing.SwingUtilities;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author jose
- */
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
